4c6adf9908e1efa54acea1ee63e0c210204eaff0
Prity Patel <pritypatel714@gmail.com>
2015-07-29 16:57:52 -0700

    edit finally working

--- app/controllers/saris_controller.rb
+++ app/controllers/saris_controller.rb
@@ -13,8 +13,30 @@ class SarisController < ApplicationController
   end
 
   def edit
-
+    @sari = Sari.find(params[:id])
   end
+
+  def update
+    @sari = Sari.find(params[:id])
+    if @sari.update_attributes(sari_params)
+    redirect_to user_path(current_user)
+    else
+    render 'edit'
+    end
+  end
+
+  # def destroy
+  #   @user = User.find_by_id(current_user.id)
+  #   sari = Sari.find(params[:id])
+  #   if sari.destroy
+  #     redirect_to current_user, notice: "Sari deleted!"
+  #   else
+  #     flash[:error] =
+  #     "Error attempting to delete sari."
+  #     redirect_to user_path(@user)
+  #   end
+  # end
+
 
   def index
     @saris = Sari.all
@@ -25,7 +47,5 @@ class SarisController < ApplicationController
     params.require(:sari).permit(:image, :story, :owner)
   end
 end
-
-
 
 
