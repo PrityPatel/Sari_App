79d8390f08a45dd75ee637d48f87b2914d6fddc0
Prity Patel <pritypatel714@gmail.com>
2015-07-30 15:29:05 -0700

    updating seed file for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,46 +9,68 @@
 User.destroy_all
 Sari.destroy_all
 
-janki_s = User.create(
-  name: "Janki",
-  email: "janki@janki.com",
-  password: "test1",
-  password_confirmation: "test1"
+prity = User.create(
+  name: "Prity",
+  email: "prity@prity.com",
+  password: "prity",
+  password_confirmation: "prity"
   )
 
-js = Sari.create(
-  image: "http://sareeblousefashion.com/wp-content/uploads/2011/02/Vidya-Balan-in_Off_Shoulder_Quarter_Sleeves_Saree_Blouse.jpg",
-  story: "This is a test story of Vidya Balan in a Sari.",
-  owner: "Janki S."
+p1 = Sari.create(
+  image: "http://l.yimg.com/jj/image-4d50e6c60fe12-sari7_080211_620.jpg",
+  story: "Picture of a red sari",
+  owner: "Mom"
   )
 
-sadhana_k = User.create(
-  name: "Sadhana",
-  email: "sadhana@sadhana.com",
-  password: "test12",
-  password_confirmation: "test12",
+p2 = Sari.create(
+  image: "https://lh6.googleusercontent.com/-OjE_ZpY-p-Y/TXC3JjhTHcI/AAAAAAAAIZo/mIct0LBGjc8/s1600/hq-genelia-saree-911.jpg",
+  story: "Pretty in Pink",
+  owner: "Genelia"
   )
 
-sk = Sari.create(
-  image: "http://cache23.fansshare.com/celebrity/photos/934_preity-zinta-red-hot-saree-if-in-saree-675681834.jpg",
-  story: "This is a test image of Preity Zinta in a Sari",
-  owner: "Sadhana K."
+tiffany = User.create(
+  name: "Tiffany",
+  email: "tiffany@tiffany.com",
+  password: "tiffany",
+  password_confirmation: "tiffany"
   )
 
-rupali_p = User.create(
-  name: "Rupali",
-  email: "rupali@rupali.com",
-  password: "test123",
-  password_confirmation: "test123"
+t1 = Sari.create(
+  image: "https://s-media-cache-ak0.pinimg.com/236x/51/7f/52/517f525306f3febe4c12dca7f1acc672.jpg",
+  story: "Sari with Japanese influence",
+  owner: "Rachelle"
+  )
+t2 = Sari.create(
+  image: "http://lh4.ggpht.com/_ZrUz79UszOA/TZ1x8-nLIzI/AAAAAAAAA7s/QIi_bq8IjU8/Tarun_Tahiliani_Tan_Saree_thumb%5B1%5D.jpg?imgmax=800
+",
+  story: "Sari with Chinese embroidery",
+  owner: "Tarun"
   )
 
-rp = Sari.create(
-  image: "http://cache23.fansshare.com/celebrity/photos/934_preity-zinta-red-hot-saree-if-in-saree-675681834.jpg",
-  story: "This is a test image of Preity Zinta in a Sari",
-  owner: "Rupali P."
+rachelle = User.create(
+  name: "Rachelle",
+  email: "rachelle@rachelle.com",
+  password: "rachelle",
+  password_confirmation: "rachelle"
+  )
+
+r1 = Sari.create(
+  image: "http://cache2.asset-cache.net/gc/517071525-young-indian-woman-in-desert-village-gettyimages.jpg?v=1&c=IWSAsset&k=2&d=sfjBaqs71C2%2BxNlbYUJGYXX7O%2BZUz0Zw6IQ1i8zm9TWk1%2Fo6VhA%2Bh8g9aXYnhp1Q%2B7ETkUtia1vyR2iztSo6Fw%3D%3D",
+  story: " Woman in the Desert",
+  owner: "Getty"
+  )
+
+r2 = Sari.create(
+  image: "http://cache2.asset-cache.net/gc/79532201-smiling-woman-in-traditional-rajasthani-sari-gettyimages.jpg?v=1&c=IWSAsset&k=2&d=acREyNHKzEwbrs6l%2B3kVY85K%2Fw%2B%2Bi0bVgz%2FmqdGP%2FyYhSlxAhrQ8xbk6K0x0V%2BsQ",
+  story: " Smiling Rajasthani Woman",
+  owner: "Getty"
   )
 
 
-janki_s.saris.push js
-sadhana_k.saris.push sk
-rupali_p.saris.push rp
+
+prity.saris.push p1
+prity.saris.push p2
+tiffany.saris.push t1
+tiffany.saris.push t2
+rachelle.saris.push r1
+rachelle.saris.push r2
